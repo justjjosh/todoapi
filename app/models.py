23418f9f5e8094682a70f1d32bd5261b10a334e6
josh <rajiolatubosun@gmail.com>
2026-01-24 23:59:25 +0100

    added the models for the users table and updated dependencies

--- app/models.py
+++ app/models.py
@@ -10,3 +10,12 @@ class Todo(Base):
     description = Column(String, nullable=True)
     completed = Column(Boolean, default=False)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
+
+class Users(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, nullabe=False, index=True)
+    email = Column(String, unique=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
